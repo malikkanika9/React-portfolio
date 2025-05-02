eb5344940550838eb8bb5a5d617a338c71f616dd
kanikaoctet <kanika@octet.design>
2025-05-02 12:54:16 +0530

    done

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -18,7 +18,7 @@ import Works from "../components/homepage/works";
 import AllProjects from "../components/projects/allProjects";
 
 import INFO from "../data/user";
-import SEO from "../data/seo";
+
 import myArticles from "../data/articles";
 
 import "./styles/homepage.css";
@@ -56,8 +56,7 @@ const Homepage = () => {
 		return () => window.removeEventListener("scroll", handleScroll);
 	}, [logoSize, oldLogoSize]);
 
-	const currentSEO = SEO.find((item) => item.page === "home");
-
+	
 	const logoStyle = {
 		display: "flex",
 		position: stayLogo ? "fixed" : "relative",
@@ -72,11 +71,7 @@ const Homepage = () => {
 		<React.Fragment>
 			<Helmet>
 				<title>{INFO.main.title}</title>
-				<meta name="description" content={currentSEO.description} />
-				<meta
-					name="keywords"
-					content={currentSEO.keywords.join(", ")}
-				/>
+				
 			</Helmet>
 
 			<div className="page-content">
